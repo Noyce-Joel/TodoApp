6ce7703aff2bb2aa1535b10cf59d806346995843
Joel Noyce <noyce.joel@gmail.com>
2024-02-21 12:56:06 +0000

    fix

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const url =
   process.env.MONGODB_URI
 
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Error connecting to MongoDB:', error));
 
